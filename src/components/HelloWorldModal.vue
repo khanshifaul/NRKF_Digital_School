e17ceca5079bd804ebc42eb160447811025134d1
Shifaul Islam <khanshifaul@gmail.com>
2022-12-02 00:31:19 +0600

    new file:   src/assets/img/admission_leaflet.jpg 	modified:   src/components/HelloWorldModal.vue

--- src/components/HelloWorldModal.vue
+++ src/components/HelloWorldModal.vue
@@ -15,9 +15,9 @@ function closeModal() {
                         <Icon class="text-4xl text-white absolute -top-3 -right-3" icon="mdi:close-circle"
                             @click="closeModal()" />
                     </div>
-                    <div class="grid grid-rows-[1fr_auto] gap-2 p-3 bg-blue-300 h-fit w-80 md:w-fit">
+                    <div class="grid grid-rows-[1fr_auto] gap-2 p-3 bg-blue-300 rounded h-fit w-80 md:w-fit">
                         <div class="flex flex-col items-center justify-center">
-                            <img class="object-cover h-fit" src="@/assets/img/carousel-1.svg" alt="">
+                            <img class="object-cover max-h-[80vh]" src="@/assets/img/admission_leaflet.jpg" alt="">
                         </div>
                         <div class="flex items-end justify-center">
                             <router-link class="bg-[#FB2576] p-3 rounded font-semibold"
